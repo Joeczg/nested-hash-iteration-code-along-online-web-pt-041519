15b8e17b777fd77ce72c9232765934b5446b58b4
zhenggao cai <caizhengao1992@gmail.com>
2019-04-25 04:46:25 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,8 @@ require 'pry'
   
 def remove_strawberry(contacts)
 contacts.each do |person, contacts_details_hash|
+  contacts_details_hash.each do |attribute, data|
+    if attribute == :favorite_ice_cream_flavors
   binding.pry
 end
 end
